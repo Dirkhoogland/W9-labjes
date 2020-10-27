7c8c619d5aefc6d7a232592acb2b552ec6d8d3dd
Dirkhoogland <99063413@mydavinci.nl>
2020-10-27 11:55:20 +0100

    labje 1 opdracht 3

--- functie3.js
+++ functie3.js
@@ -1,9 +1,40 @@
 
 
-function naamenleeftijd (naam, leeftijd) {
+function start(naam, leeftijd) {
+			var naam = prompt("wat is je naam ");
+			var leeftijd = prompt("leeftijd");
+			document.write ("dit is je naam " + naam +" dit is je leeftijd " + leeftijd);
+			var klaar = prompt("verder of stop");
+			console.log(naam + " en " +  leeftijd + " en " + klaar);
 
-			 document.write ("dit is je naam " + naam +"dit is je leeftijd " + leeftijd);
-	
 
 
-}var naamenleeftijd = prompt("wat is je naam en leeftijd?");
+			if (klaar == "stop") { alert("bedankt voor het gebruik");}
+			else if  (klaar == "verder") { 			
+				var naam = prompt("wat is je naam ");
+				var leeftijd = prompt("leeftijd");
+				document.write ("dit is je naam " + naam +" dit is je leeftijd " + leeftijd)
+				var klaar = prompt("verder of stop");
+				console.log(naam + " en " +  leeftijd + " en " + klaar);
+
+					if (klaar == "stop") { alert("bedankt voor het gebruik");}
+					else if  (klaar == "verder") { 			
+					var naam = prompt("wat is je naam ");
+					var leeftijd = prompt("leeftijd");
+					document.write ("dit is je naam " + naam +" dit is je leeftijd " + leeftijd)
+					var klaar = prompt("verder of stop");
+					console.log(naam + " en " +  leeftijd + " en " + klaar);
+
+							if (klaar == "stop") { alert("bedankt voor het gebruik");}
+							else if  (klaar == "verder") { 			
+							var naam = prompt("wat is je naam ");
+							var leeftijd = prompt("leeftijd");
+							document.write ("dit is je naam " + naam +" dit is je leeftijd " + leeftijd)
+							var klaar = prompt("verder of stop");
+							console.log(naam + " en " +  leeftijd + " en " + klaar);
+
+							} 
+					} 
+
+			}
+}
