153681dcc79dc8eb5aa4256d98ac5353beb5a231
Dirkhoogland <99063413@mydavinci.nl>
2020-10-27 14:13:02 +0100

    lab 4 af

--- functie4.js
+++ functie4.js
@@ -1,9 +1,27 @@
 
 function getal () {
-	var nummer = prompt("welk getal wil je weten");
-	for (var i = 0; i <= nummer i++) { 
-		document.write(1 * nummer);
+	var nummers = prompt("U dient een getal in te voeren ");
+	
+	if (nummers >= 11 ) { alert ("Bereik van getal moet zijn: 1 t/m 10");}
+
+		else if (nummers <= 10 ) { 
 		
-	}
+
+			for (var a=1;  a <= nummers; a++ ) {
+		 				for (var i = 1; i <= 10; i++) {
+		
+		
+						document.write( i + " x " + a + " = " + i * a + "<br>"); 
+						}
+						document.write("<hr>");
+			} 
+		}
+
 
 }
+		
+	
+
+
+
+getal();
